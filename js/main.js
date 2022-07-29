a70483decfb36a729f3128fcdae08f3641fe1b63
Doom <vnagorniyrnd@gmail.com>
2022-07-29 20:36:00 +0300

    Верстка блока section-Customers

--- js/main.js
+++ js/main.js
@@ -1,9 +1,16 @@
-$(document).ready(function () {
-  $('.owl-carousel').owlCarousel({
-    center: true,
-    loop: true,
-    margin: 30,
-    startPosition: 1,
-    item: 3,
-  });
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+  center: true,
+  loop: true,
+  margin: 30,
+  startPosition: 1,
+  items: 3,
+});
+
+$('.slider__btn--prev').click(function () {
+  owl.trigger('prev.owl.carousel');
+});
+
+$('.slider__btn--next').click(function () {
+  owl.trigger('next.owl.carousel');
 });
